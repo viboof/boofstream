77fbb17fd3d58bbc0b8b585a7103703500ed13c6
viboof <boof@viboof.com>
2025-01-21 23:03:55 -0800

    idk why this doesnt work

--- scripts/make-release-artifact.sh
+++ scripts/make-release-artifact.sh
@@ -6,9 +6,9 @@ NODE_TARGET=node20
 VERSION=$(git describe --tags)
 
 cd boofstream
-pkg -t "$NODE_TARGET"-"$1" dist/index.js -o dist/boofstream
+pkg -t "$NODE_TARGET"-"$1" dist/index.js -o dist/boofstream"$3"
 cd ../release
-cp ../boofstream/dist/boofstream boofstream"$3"
+cp ../boofstream/dist/boofstream"$3" boofstream"$3"
 zip -r ../boofstream-"$VERSION"-"$2".zip *
 rm boofstream"$3"
 cd ..
