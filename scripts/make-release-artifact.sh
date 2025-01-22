e84109ccd28b99de9c639df519b922f6c8e9b3ab
viboof <boof@viboof.com>
2025-01-21 22:49:04 -0800

    simplify release artifacts

---
+++ scripts/make-release-artifact.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# usage: ./scripts/make-release-artifact.sh win-x64 windows-x64 .exe
+# for making a win-x64 artifact named boofstream.exe in zip boofstream-v1.0.0-windows-x64.zip
+
+NODE_TARGET=node20
+VERSION=$(git describe --tags)
+
+cd boofstream
+pkg -t "$NODE_TARGET"-"$1" dist/index.js -o dist/boofstream
+cd ../release
+cp ../boofstream/dist/boofstream boofstream"$3"
+zip -r ../boofstream-"$VERSION"-"$2".zip *
+rm boofstream"$3"
+cd ..
